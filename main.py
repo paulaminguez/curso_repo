7065033ed32aa879aa51e12205a33d56c062245e
paulaminguez <paula.minmu@gmail.com>
2025-08-31 16:38:54 +0200

    pruebo a añadir una linea

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}. Esto es nuevo. Si lo guardo qué')  # Press ⌘F8 to toggle the breakpoint.
+    print("linea 2")
+    print("linea 3")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
